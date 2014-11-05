d1cee0978d6d2095ae885fc8e7603e0f0e28bc3e
Aref Aslani <arefaslani@gmail.com>
2014-11-05 14:54:16 +0330

    create engine class

--- lib/bootstrap_farsi/engine.rb
+++ lib/bootstrap_farsi/engine.rb
@@ -1,5 +1,7 @@
 module BootstrapFarsi
   class Engine < ::Rails::Engine
-    
+    initializer 'bootstrap_farsi.add_less_path' do |app|
+      app.config.less.paths << File.join(config.root, 'vendor', 'assets', 'less')
+    end
   end
 end
